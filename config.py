d1997186728722df52f7088dc3d1bc1aa1ba3178
jcarrasquet <jcarrasquet@gmail.com>
2025-04-17 21:49:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-WC_API_URL = 'https://idyllica.com/wp-json/wc/v3/products'
-CONSUMER_KEY = 'ck_tu_clave'
-CONSUMER_SECRET = 'cs_tu_secreta'
-CLAVE_GOOGLE = 'clave_google.json'
+WC_API_URL = 'https://idyllica.es/wp-json/wc/v3/products'
+CONSUMER_KEY = 'ck_dc4d1643bd3496975df7ebb675a9c95ad840b2cb'
+CONSUMER_SECRET = 'cs_cb8dc8667197d0cdc17ba5943a18b93367497449'
+CLAVE_GOOGLE = 'seo-idyllica-017e941a4cb1'
 HISTORIAL_CSV = 'enviados_a_google.csv'
-N_URLS_DIARIAS = 10
+N_URLS_DIARIAS = 30
